2d3cdbb7fcd96afd96e1bc1c629b0a5d164b4469
Jason <jason.lm@live.com>
2019-08-20 09:57:07 -0700

    1

--- UnitConverter/ViewController.swift
+++ UnitConverter/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tempText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        tempText.endEditing(true)
+    }
 
-
+    @IBAction func convertTemp(_ sender: UIButton) {
+        guard let tempString = tempText.text else {return}
+        if let fahrenheit = Double(tempString) {
+            let celsius = (fahrenheit - 32)/1.8
+            let resultText = "Celsius \(celsius)"
+            resultLabel.text = resultText
+        }
+    }
+    
+    @IBAction func textFieldReturn(_ sender: UITextField) {
+        _ = resignFirstResponder()
+    }
 }
-
